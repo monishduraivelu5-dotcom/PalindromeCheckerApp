cf075aebef16f833a9c9a0e86715d9200a6c52dc
aposcus <ritiknegimail@gmail.com>
2026-02-16 15:49:47 +0530

    UC1: Application Entry & Welcome Message

---
+++ PalindromeCheckerApp1/src/PalindromeCheckerApp.java
@@ -0,0 +1,18 @@
+/*
+main class usercase1plaindrome
+
+no plaindrome logic implemented yet
+
+
+@monish authour
+version 1.0
+ */
+
+
+public class PalindromeCheckerApp {
+    public static void main(String[] args) {
+        System.out.println("palindromeCheckerApp");
+        System.out.println("version 1.0");
+        System.out.println("system initialized successfully");
+    }
+}
