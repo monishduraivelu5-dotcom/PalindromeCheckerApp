7683da261466be962a5004a8755dd9f9174abebe
monish <monishduraivelu5@gmail.com>
2026-03-10 14:13:02 +0530

    uc2

--- PalindromeCheckerApp1/src/PalindromeCheckerApp.java
+++ PalindromeCheckerApp1/src/PalindromeCheckerApp.java
@@ -1,18 +1,22 @@
-/*
-main class usercase1plaindrome
+public class UseCase2PalindromeCheckerApp {
 
-no plaindrome logic implemented yet
-
-
-@monish authour
-version 1.0
- */
-
-
-public class PalindromeCheckerApp {
     public static void main(String[] args) {
-        System.out.println("palindromeCheckerApp");
-        System.out.println("version 1.0");
-        System.out.println("system initialized successfully");
+
+        String input = "madam";   // Hardcoded string
+        boolean isPalindrome = true;
+
+        // Loop only till half of the string length
+        for (int i = 0; i < input.length() / 2; i++) {
+            if (input.charAt(i) != input.charAt(input.length() - 1 - i)) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        if (isPalindrome) {
+            System.out.println(input + " is a Palindrome.");
+        } else {
+            System.out.println(input + " is NOT a Palindrome.");
+        }
     }
 }
